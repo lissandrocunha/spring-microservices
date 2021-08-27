54c3292b1ce6f02da2e68a951145fbf73a032ed6
Lissandro Perossi Cunha <lissandro_cunha@yahoo.com.br>
2021-08-27 13:26:30 -0300

    create project with basic

---
+++ src/microservice-task/src/main/java/io/lissandrocunha/task/MicroserviceTaskApplication.java
@@ -0,0 +1,13 @@
+package io.lissandrocunha.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTaskApplication.class, args);
+	}
+
+}
